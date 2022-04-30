4744f6d39a6496e775c013e00c3a2f38adff68e4
Arsen Nagdalian <nagoarsen@mail.ru>
2022-04-30 12:25:52 +0300

    Add IO streams customization to command factories

--- src/ru/itmo/sd/shell/cli/command/AbstractCommandFactory.kt
+++ src/ru/itmo/sd/shell/cli/command/AbstractCommandFactory.kt
@@ -1,5 +1,8 @@
 package ru.itmo.sd.shell.cli.command
 
+import java.io.InputStream
+import java.io.OutputStream
+
 object CommandFactoryHandler {
     fun getFactoryFor(commandName: String): AbstractCommandFactory =
         when (commandName) {
@@ -14,33 +17,65 @@ object CommandFactoryHandler {
 }
 
 abstract class AbstractCommandFactory {
-    abstract fun createCommand(arguments: List<String>): CliSimpleCommand
+    abstract fun createCommand(
+        arguments: List<String> = emptyList(),
+        inputStream: InputStream = System.`in`,
+        outputStream: OutputStream = System.out
+    ): CliSimpleCommand
 }
 
 class CatCommandFactory : AbstractCommandFactory() {
-    override fun createCommand(arguments: List<String>): CliSimpleCommand = CatCommand(arguments)
+    override fun createCommand(
+        arguments: List<String>,
+        inputStream: InputStream,
+        outputStream: OutputStream
+    ): CliSimpleCommand = CatCommand(arguments, inputStream, outputStream)
 }
 
 class EchoCommandFactory : AbstractCommandFactory() {
-    override fun createCommand(arguments: List<String>): CliSimpleCommand = EchoCommand(arguments)
+    override fun createCommand(
+        arguments: List<String>,
+        inputStream: InputStream,
+        outputStream: OutputStream
+    ): CliSimpleCommand = EchoCommand(arguments, inputStream, outputStream)
 }
 
 class ExitCommandFactory : AbstractCommandFactory() {
-    override fun createCommand(arguments: List<String>): CliSimpleCommand = ExitCommand(arguments)
+    override fun createCommand(
+        arguments: List<String>,
+        inputStream: InputStream,
+        outputStream: OutputStream
+    ): CliSimpleCommand = ExitCommand(arguments, inputStream, outputStream)
 }
 
 class GrepCommandFactory : AbstractCommandFactory() {
-    override fun createCommand(arguments: List<String>): CliSimpleCommand = CatCommand(arguments)
+    override fun createCommand(
+        arguments: List<String>,
+        inputStream: InputStream,
+        outputStream: OutputStream
+    ): CliSimpleCommand = GrepCommand(arguments, inputStream, outputStream)
 }
 
 class PwdCommandFactory : AbstractCommandFactory() {
-    override fun createCommand(arguments: List<String>): CliSimpleCommand = PwdCommand(arguments)
+    override fun createCommand(
+        arguments: List<String>,
+        inputStream: InputStream,
+        outputStream: OutputStream
+    ): CliSimpleCommand = PwdCommand(arguments, inputStream, outputStream)
 }
 
 class WcCommandFactory : AbstractCommandFactory() {
-    override fun createCommand(arguments: List<String>): CliSimpleCommand = WcCommand(arguments)
+    override fun createCommand(
+        arguments: List<String>,
+        inputStream: InputStream,
+        outputStream: OutputStream
+    ): CliSimpleCommand = WcCommand(arguments, inputStream, outputStream)
 }
 
 class ExternalCommandFactory(private val commandName: String) : AbstractCommandFactory() {
-    override fun createCommand(arguments: List<String>): CliSimpleCommand = ExternalCommand(commandName, arguments)
+    override fun createCommand(
+        arguments: List<String>,
+        inputStream: InputStream,
+        outputStream: OutputStream
+    ): CliSimpleCommand = ExternalCommand(arguments, commandName, inputStream, outputStream)
 }
