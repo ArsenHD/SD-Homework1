c2b038ad6ce82bbe06317f189ea25f87249e1e4c
Arsen Nagdalian <nagoarsen@mail.ru>
2022-04-30 19:18:32 +0300

    Remove unused exceptions

--- src/ru/itmo/sd/shell/exception/Exceptions.kt
+++ src/ru/itmo/sd/shell/exception/Exceptions.kt
@@ -1,17 +1,6 @@
 package ru.itmo.sd.shell.exception
 
-import ru.itmo.sd.shell.parser.Token
-
-class UndefinedVariableException(name: String) : Exception("Variable '$name' is not defined")
-
-class UnexpectedTokenException(token: Token) : Exception("Token $token was not expected")
-
-class IllegalCommandArgumentException(override val message: String?) : Exception()
-
-class ExecutionFailureError(
-    exitCode: Int,
-    override val cause: Throwable? = null
-) : Exception("Execution terminated with exit code $exitCode")
+class ExecutionFailureError(exitCode: Int) : Exception("Execution failed with exit code $exitCode")
 
 abstract class SyntaxError(override val message: String?) : Exception()
 
