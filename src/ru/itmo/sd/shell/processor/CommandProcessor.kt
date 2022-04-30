270396e11011790ee44afbdf2fa95a123498e3c6
Arsen Nagdalian <nagoarsen@mail.ru>
2022-04-30 12:32:32 +0300

    Fix main loop error isolation

--- src/ru/itmo/sd/shell/processor/CommandProcessor.kt
+++ src/ru/itmo/sd/shell/processor/CommandProcessor.kt
@@ -5,7 +5,6 @@ import ru.itmo.sd.shell.cli.util.executorService
 import ru.itmo.sd.shell.environment.Environment
 import ru.itmo.sd.shell.exception.ShellShutdownException
 import ru.itmo.sd.shell.exception.SyntaxError
-import ru.itmo.sd.shell.parser.CommandLexer
 import ru.itmo.sd.shell.parser.CommandParser
 import java.io.InputStream
 import java.io.OutputStream
@@ -15,20 +14,23 @@ class CommandProcessor(inputStream: InputStream, outputStream: OutputStream) {
     private val environment = Environment()
     private val parser = CommandParser.getInstance(inputStream, outputStream, environment)
 
-    private val parser = CommandParser(inputStream, outputStream, lexer)
-
-    fun run() = runCatching {
-        var element = parser.parse()
-        while (element != null) {
-            process(element)
-            element = parser.parse()
-        }
-        finish()
-    }.onFailure {
-        when (it) {
-            is SyntaxError -> println("shell: syntax error: ${it.message}")
-            is ShellShutdownException -> Unit
-            is Exception -> println("shell: error: failed to execute command")
+    fun run() {
+        while (true) {
+            try {
+                var element = parser.parse()
+                while (element != null) {
+                    process(element)
+                    element = parser.parse()
+                }
+                finish()
+                break
+            } catch (e: SyntaxError) {
+                println("shell: syntax error: ${e.message}")
+            } catch (e: ShellShutdownException) {
+                break
+            } catch (e: Exception) {
+                println("shell: error: failed to execute command")
+            }
         }
     }
 
